5b089de6043663cc322311c2678a546fe93905ec
abusov <146867646+abusoww@users.noreply.github.com>
2024-11-12 18:04:06 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,33 @@
 function increment(id) {
-    let input = document.getElementById(id);
+    var input = document.getElementById(id);
     input.value = parseInt(input.value) + 1;
 }
 
 function decrement(id) {
-    let input = document.getElementById(id);
-    if (parseInt(input.value) > 0) {
+    var input = document.getElementById(id);
+    if (input.value > 0) {
         input.value = parseInt(input.value) - 1;
     }
 }
 
-function calculateScore() {
-    let openMath = parseInt(document.getElementById('open-questions-math').value);
-    let closedMath = parseInt(document.getElementById('closed-questions-math').value);
-    let codingMath = parseInt(document.getElementById('coding-questions-math').value);
-    
-    let openRussian = parseInt(document.getElementById('open-questions-russian').value);
-    let closedRussian = parseInt(document.getElementById('closed-questions-russian').value);
+function calculateScores() {
+    var mathOpen = parseInt(document.getElementById('math_open').value);
+    var mathClosed = parseInt(document.getElementById('math_closed').value);
+    var mathCoded = parseInt(document.getElementById('math_coded').value);
+    var russianOpen = parseInt(document.getElementById('russian_open').value);
+    var russianClosed = parseInt(document.getElementById('russian_closed').value);
+    var englishOpen = parseInt(document.getElementById('english_open').value);
+    var englishClosed = parseInt(document.getElementById('english_closed').value);
 
-    let openEnglish = parseInt(document.getElementById('open-questions-english').value);
-    let closedEnglish = parseInt(document.getElementById('closed-questions-english').value);
+    // Формулы для расчета баллов
+    var mathScore = (25 / 8) * (2 * mathOpen + mathClosed + mathCoded);
+    var russianScore = 2.5 * (2 * russianOpen + russianClosed);
+    var englishScore = (100 / 37) * (2 * englishOpen + englishClosed);
 
-    // Математика
-    let mathScore = (25 / 8) * (2 * openMath + closedMath + codingMath);
+    // Вывод результатов
+    var result = "Математика: " + mathScore.toFixed(2) + "<br>" +
+                 "Русский язык: " + russianScore.toFixed(2) + "<br>" +
+                 "Английский язык: " + englishScore.toFixed(2);
 
-    // Русский язык
-    let russianScore = 2.5 * (2 * openRussian + closedRussian);
-
-    // Английский язык
-    let englishScore = (100 / 37) * (2 * openEnglish + closedEnglish);
-
-    let totalScore = mathScore + russianScore + englishScore;
-
-    alert("Ваши баллы: " + totalScore);
+    document.getElementById("result").innerHTML = result;
 }
