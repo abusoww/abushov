83713cd12cc8050d9081fd31797d718063dec0f4
abusov <146867646+abusoww@users.noreply.github.com>
2024-11-12 17:29:21 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function showGroup(group) {
-    const groups = ['graduation', 'group1', 'group2', 'group3', 'group4'];
+    const groups = ['graduation'];
     groups.forEach(g => {
         const groupElement = document.getElementById(g);
         if (group === g) {
@@ -10,44 +10,52 @@ function showGroup(group) {
     });
 }
 
-// Формула для Математики
+// Расчет баллов для Математики
 function calculateMathScore() {
     const openQuestions = document.getElementById('open-questions-math').value;
     const closedQuestions = document.getElementById('closed-questions-math').value;
 
     if (openQuestions === '' || closedQuestions === '') {
         alert('Пожалуйста, заполните все поля');
-        return;
+        return 0;
     }
 
-    const score = (25 / 8) * (2 * openQuestions + closedQuestions);
-    document.getElementById('math-score').textContent = `Баллы: ${score.toFixed(2)}`;
+    return (25 / 8) * (2 * openQuestions + parseInt(closedQuestions));
 }
 
-// Формула для Русского
+// Расчет баллов для Русского
 function calculateRussianScore() {
     const openQuestions = document.getElementById('open-questions-russian').value;
     const closedQuestions = document.getElementById('closed-questions-russian').value;
 
     if (openQuestions === '' || closedQuestions === '') {
         alert('Пожалуйста, заполните все поля');
-        return;
+        return 0;
     }
 
-    const score = 2.5 * (2 * openQuestions + closedQuestions);
-    document.getElementById('russian-score').textContent = `Баллы: ${score.toFixed(2)}`;
+    return 2.5 * (2 * openQuestions + parseInt(closedQuestions));
 }
 
-// Формула для Английского
+// Расчет баллов для Английского
 function calculateEnglishScore() {
     const openQuestions = document.getElementById('open-questions-english').value;
     const closedQuestions = document.getElementById('closed-questions-english').value;
 
     if (openQuestions === '' || closedQuestions === '') {
         alert('Пожалуйста, заполните все поля');
-        return;
+        return 0;
     }
 
-    const score = (100 / 37) * (2 * openQuestions + closedQuestions);
-    document.getElementById('english-score').textContent = `Баллы: ${score.toFixed(2)}`;
+    return (100 / 37) * (2 * openQuestions + parseInt(closedQuestions));
+}
+
+// Расчет всех баллов
+function calculateAllScores() {
+    const mathScore = calculateMathScore();
+    const russianScore = calculateRussianScore();
+    const englishScore = calculateEnglishScore();
+
+    document.getElementById('math-score').textContent = `Баллы по математике: ${mathScore.toFixed(2)}`;
+    document.getElementById('russian-score').textContent = `Баллы по русскому языку: ${russianScore.toFixed(2)}`;
+    document.getElementById('english-score').textContent = `Баллы по английскому языку: ${englishScore.toFixed(2)}`;
 }
